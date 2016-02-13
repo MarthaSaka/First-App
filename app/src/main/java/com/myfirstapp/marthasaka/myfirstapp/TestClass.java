88c510cc26126e06616d1585923b1c0577fb370b
Leina2015 <leinawangeshi@gmail.com>
2016-02-13 13:48:35 +0300

    first commit

---
+++ app/src/main/java/com/myfirstapp/marthasaka/myfirstapp/TestClass.java
@@ -0,0 +1,30 @@
+package com.myfirstapp.marthasaka.myfirstapp;
+
+/**
+ * Created by Student on 2/6/2016.
+ */
+public class TestClass {
+
+  public static void main(String args []){
+      //Declare variables
+      int age;
+      double shoeSize;
+      char letter;
+      String firstName;
+
+      //Initialise variables
+      age = 10;
+      shoeSize = 5.5;
+      letter = 'm';
+      firstName = "Martha Saka";
+
+      System.out.print(age);
+      System.out.println(shoeSize);
+      System.out.println(letter);
+      System.out.println(firstName);
+
+
+  }
+
+
+}
---
+++ app/src/main/java/com/myfirstapp/marthasaka/myfirstapp/mathematics.java
@@ -0,0 +1,37 @@
+package com.myfirstapp.marthasaka.myfirstapp;
+
+/**
+ * Created by Student on 2/6/2016.
+ */
+public class mathematics{
+
+    public static void main(String args[]){
+        //call a method by the method name
+        addition();
+        subtraction();
+    }
+
+        //addition method
+
+        public static void addition(){
+        int a=15;
+        int b=5;
+        int answer=a+b;
+
+        System.out.println(answer);
+    }
+    //subtraction method
+
+    public static void subtraction(){
+        int a=15;
+        int b=5;
+        int answer=a-b;
+
+        System.out.println(answer);
+    }
+
+
+    }
+
+
+
